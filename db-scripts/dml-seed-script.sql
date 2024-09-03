e3764dd6ebfc8a8246f9908665f629e81319aac3
Ricardo Oliveira <ricardoliveira5ro@gmail.com>
2024-09-03 16:31:23 +0100

    Register user with or without a club

--- db-scripts/dml-seed-script.sql
+++ db-scripts/dml-seed-script.sql
@@ -1,11 +1,5 @@
 BEGIN;
 
-    INSERT INTO users (first_name, last_name, password, contact_email, contact_phone, role, created_at, updated_at)
-    VALUES
-    ('John', 'Doe', 'password123', 'john@example.com', '123-456-7890', 'USER', NOW(), NOW()),
-    ('Jane', 'Doe', 'password456', 'jane@example.com', '098-765-4321', 'ADMIN', NOW(), NOW());
-
-
     INSERT INTO club (name, description, address, contact_email, contact_phone, created_at, updated_at)
     VALUES
     ('Sunshine Padel Club', 'A vibrant padel club with outdoor and indoor courts.', '123 Sunshine Ave', 'info@sunshinepadel.com', '123-456-7890', NOW(), NOW()),
@@ -20,11 +14,19 @@ BEGIN;
     ('Court 4', 'Artificial Grass', 'Indoor', 2, NOW(), NOW());
 
 
+    INSERT INTO users (first_name, last_name, password, contact_email, contact_phone, role, club_id, created_at, updated_at)
+    VALUES
+    ('John', 'Doe', 'password123', 'john@example.com', '123-456-7890', 'PLAYER', NULL,  NOW(), NOW()),
+    ('Jane', 'Doe', 'password456', 'jane@example.com', '098-765-4321', 'CLUB_OWNER', 1, NOW(), NOW()),
+    ('Ricardo', 'Oliveira', 'password789', 'ricardo@example.com', '098-765-4321', 'CLUB_OWNER', 2, NOW(), NOW()),
+    ('Oliveira', 'Ricardo', 'password012', 'oliveira@example.com', '123-456-7890', 'PLAYER', NULL, NOW(), NOW());
+
+
     INSERT INTO reservation (reservation_start_time, reservation_end_time, status, user_id, court_id, created_at, updated_at)
     VALUES
     ('2024-08-28 10:00:00', '2024-08-28 11:30:00', 'CONFIRMED', 1, 1, NOW(), NOW()),
-    ('2024-08-29 14:00:00', '2024-08-29 15:00:00', 'CANCELLED', 2, 2, NOW(), NOW()),
-    ('2024-08-30 09:00:00', '2024-08-30 10:30:00', 'PENDING', 1, 3, NOW(), NOW()),
-    ('2024-08-31 18:00:00', '2024-08-31 19:30:00', 'CONFIRMED', 2, 4, NOW(), NOW());
+    ('2024-08-29 14:00:00', '2024-08-29 15:00:00', 'CANCELLED', 1, 2, NOW(), NOW()),
+    ('2024-08-30 09:00:00', '2024-08-30 10:30:00', 'PENDING', 4, 3, NOW(), NOW()),
+    ('2024-08-31 18:00:00', '2024-08-31 19:30:00', 'CONFIRMED', 4, 4, NOW(), NOW());
 
 END;
--- src/main/java/com/ricardo/oliveira/padelHubAPI/dto/RegisterDTO.java
+++ src/main/java/com/ricardo/oliveira/padelHubAPI/dto/RegisterDTO.java
@@ -9,55 +9,37 @@ public class RegisterDTO {
     private String lastName;
     private String email;
     private String contactPhone;
+    private RegisterClubDTO club;
 
     @Pattern(regexp = "ADMIN|CLUB_OWNER|PLAYER", message = "Invalid role. Allowed values are ADMIN, CLUB_OWNER, PLAYER")
     private String role;
 
+
     public String getPassword() {
         return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
     }
 
     public String getFirstName() {
         return firstName;
     }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
     public String getLastName() {
         return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
     }
 
     public String getEmail() {
         return email;
     }
 
-    public void setContactEmail(String email) {
-        this.email = email;
-    }
-
     public String getContactPhone() {
         return contactPhone;
-    }
-
-    public void setContactPhone(String contactPhone) {
-        this.contactPhone = contactPhone;
     }
 
     public String getRole() {
         return role;
     }
 
-    public void setRole(String role) {
-        this.role = role;
+    public RegisterClubDTO getClub() {
+        return club;
     }
 }
