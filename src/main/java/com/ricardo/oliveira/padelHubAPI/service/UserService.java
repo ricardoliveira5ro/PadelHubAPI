533ca5092821b368fdd79435b3b0377660f94933
Ricardo Oliveira <ricardoliveira5ro@gmail.com>
2024-09-04 14:22:04 +0100

    Remove unnecessary service methods in user repository

--- src/main/java/com/ricardo/oliveira/padelHubAPI/service/UserService.java
+++ src/main/java/com/ricardo/oliveira/padelHubAPI/service/UserService.java
@@ -3,15 +3,8 @@ package com.ricardo.oliveira.padelHubAPI.service;
 import com.ricardo.oliveira.padelHubAPI.dto.LoginDTO;
 import com.ricardo.oliveira.padelHubAPI.dto.RegisterDTO;
 import com.ricardo.oliveira.padelHubAPI.model.User;
-import java.util.List;
 
 public interface UserService {
-
-    List<User> findAll();
-
-    User findById(Integer id);
-
-    User save(User user);
 
     User signup(RegisterDTO registerDTO);
 
